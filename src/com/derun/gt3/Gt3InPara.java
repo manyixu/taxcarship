a932cff42bdad9f4e5979d99075484f9c0ec0c28
wbzhao <wbzhao7@gmail.com>
2016-05-23 17:38:34 +0800

    查询金三完税、减免税、退税信息

---
+++ src/com/derun/gt3/Gt3InPara.java
@@ -0,0 +1,19 @@
+package com.derun.gt3;
+
+/**
+ * 金三数据接口入参
+ *
+ */
+public class Gt3InPara {
+	
+	String vin;
+
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+
+}
---
+++ src/com/derun/gt3/Gt3OutPara.java
@@ -0,0 +1,31 @@
+package com.derun.gt3;
+
+/**
+ * 金三数据接口出参
+ *
+ */
+public class Gt3OutPara {
+	
+	String vin;
+//	a.sbxxuuid,
+//    c.djxh,
+//    c.nsrsbh,
+//    b.clsbdh CCSBDM,
+//    b.clpzh CLHPHM,
+//    '' CLHPZL,
+//    '' CLLX,
+//    c.nsrmc SYRMC,
+//    a.pzxh,
+//    a.skssqq,
+//    a.skssqz,
+//    '0' cclx
+
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+
+}
