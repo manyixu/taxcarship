438f5992ae654b0def4980cc861aa0971f212d11
wbzhao <wbzhao7@gmail.com>
2016-05-27 17:33:50 +0800

    gt3v2

--- src/com/derun/gt3/Gt3QueryUtil.java
+++ src/com/derun/gt3/Gt3QueryUtil.java
@@ -36,33 +36,34 @@ public class Gt3QueryUtil {
 		SYJK_CCS_WSDJXX wsdj = new SYJK_CCS_WSDJXX();
 		
 		Gt3DBUtil gt3DBUtil = Gt3DBUtil.getInstance();
+		//目前提供的查询sql只有代征单位代码，没有开具完税凭证税务机关代码和税务机关名称，税务机关名称需要关联字典表查询
+		StringBuffer sb = new StringBuffer();
 		
-		String sql = "select SWSX_DM from DJ_ZRR where 1=1 and b.clsbdh = '"+wsIn.getVin()+"'";
-		SSRS ssrs = gt3DBUtil.Query(sql);	
+		SSRS ssrs = gt3DBUtil.Query(sb.toString());	
 		
 		try {
-			wsdj.setID("");
-			wsdj.setNSRSBH(ssrs.GetText(1, 1));//c.nsrsbh
-			wsdj.setLOGINNAME("");
-			wsdj.setREVENUECODE("");
-			wsdj.setSJCJRQ(sdfd.parse(ssrs.GetText(1, 2)));//sjcjrq数据采集日期
-			wsdj.setSJCJFS("");
-			wsdj.setCLHPHM(ssrs.GetText(1, 3));//hphm号牌号码
-			wsdj.setCLHPZL("");
-			wsdj.setCLLX("");
-			wsdj.setSYRMC(ssrs.GetText(1, 4));//mc所有人名称
-			wsdj.setKJSWJGDM(ssrs.GetText(1, 5));//swjgdm开具凭证税务机关代码
-			wsdj.setZGSWJGMC("");
-			wsdj.setCCSBDM(ssrs.GetText(1, 6));//VIN车架号
-			wsdj.setDZDWMC("");
-			wsdj.setDZDWDM(ssrs.GetText(1, 7));//代征单位代码
-			wsdj.setWSPZH(ssrs.GetText(1, 8));//wspzh完税凭证号
-			wsdj.setSKSSKSRQ(sdfs.parse(ssrs.GetText(1, 9)));//skssqq税款所属起期
-			wsdj.setSKSSJSRQ(sdfs.parse(ssrs.GetText(1, 10)));//sksszq税款所属止期
-			wsdj.setJNJE(Double.parseDouble(ssrs.GetText(1, 11)));//je缴纳金额
-			wsdj.setWSRQ(sdfs.parse(ssrs.GetText(1, 12)));//wsrq完税日期
-			wsdj.setSKLX("");
-			wsdj.setZSPM("");
+			wsdj.setID(ssrs.GetText(1, 1));
+			wsdj.setNSRSBH(ssrs.GetText(1, 2));//c.nsrsbh
+//			wsdj.setLOGINNAME("");
+//			wsdj.setREVENUECODE("");
+//			wsdj.setSJCJFS("");
+			wsdj.setCCSBDM(ssrs.GetText(1, 3));//VIN车架号
+			wsdj.setCLHPHM(ssrs.GetText(1, 4));//hphm号牌号码
+			wsdj.setCLHPZL(ssrs.GetText(1, 5));
+			wsdj.setCLLX(ssrs.GetText(1, 6));
+			wsdj.setSYRMC(ssrs.GetText(1, 7));//mc所有人名称
+			wsdj.setSKSSKSRQ(sdfs.parse(ssrs.GetText(1, 8)));//skssqq税款所属起期
+			wsdj.setSKSSJSRQ(sdfs.parse(ssrs.GetText(1, 9)));//sksszq税款所属止期
+			wsdj.setJNJE(Double.parseDouble(ssrs.GetText(1, 10)));//je缴纳金额
+			wsdj.setWSRQ(sdfs.parse(ssrs.GetText(1, 11)));//wsrq完税日期
+			wsdj.setDZDWDM(ssrs.GetText(1, 12));//代征单位代码
+			wsdj.setWSPZH(ssrs.GetText(1, 13));//wspzh完税凭证号***
+			wsdj.setSJCJRQ(sdfd.parse(ssrs.GetText(1, 14)));//sjcjrq数据采集日期
+			wsdj.setKJSWJGDM(ssrs.GetText(1, 15));//swjgdm开具凭证税务机关代码***
+			wsdj.setZGSWJGMC(ssrs.GetText(1, 16));//主管税务机关名称***
+//			wsdj.setDZDWMC("");
+//			wsdj.setSKLX("");
+//			wsdj.setZSPM("");
 		} catch (ParseException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
